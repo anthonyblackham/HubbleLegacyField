b329eebd0f1b45093075c584c49c5744b536d13a
anthonyblackham <anthonyblackham@gmail.com>
2019-05-12 01:26:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,7 @@
     // the tile layer containing the image generated with gdal2tiles --leaflet ...
     L.tileLayer('https://raw.githubusercontent.com/anthonyblackham/HubbleLegacyField/master/tiles/{z}/{x}/{y}.png', {
       noWrap: true,
-      attribution: 'Map <a href="http://hubblesite.org/news_release/news/2019-17' +
-        'Hubble Legacy Field</a> under ' +
+      attribution: 'Map <a href="http://hubblesite.org/news_release/news/2019-17>Hubble Legacy Field</a> under ' +
         '<a href="https://creativecommons.org/publicdomain/zero/1.0/deed.en">CC0</a>'
     }).addTo(map)
     
